bb0bf28ae9b6eebcca2ea2616bf701486fd84bb5
Ginger Chody <chodness@gmail.com>
2026-02-05 11:12:32 -0800

    Add console app project
    Just there to test the parsers for now, but will later actually do the conversions

---
+++ GLinkConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GLink.Helpers;
+using GLink.Immutable;
+using Revrs;
+
+RevrsReader rev = new(File.ReadAllBytes("/home/Ginger/Documents/CemuShit/SLink2DB.bslnk"));
+XLinkReader reader = new(ref rev);
+XLinkLoader loader = new(ref reader);
+Console.WriteLine("Loaded WiiU SLink!");
+
+rev = new(File.ReadAllBytes("/home/Ginger/Documents/CemuShit/ELink2DB.belnk"));
+reader = new(ref rev);
+loader = new(ref reader);
+Console.WriteLine("Loaded WiiU ELink!");
+
+rev = new(File.ReadAllBytes("/home/Ginger/Documents/CemuShit/actually_switch/SLink2DB.bslnk"));
+reader = new(ref rev);
+loader = new(ref reader);
+Console.WriteLine("Loaded Switch SLink!");
+
+rev = new(File.ReadAllBytes("/home/Ginger/Documents/CemuShit/actually_switch/ELink2DB.belnk"));
+reader = new(ref rev);
+loader = new(ref reader);
+Console.WriteLine("Loaded Switch ELink!");
