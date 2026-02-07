fa54399784dd079e7665b710d5fe4ecdba24a99d
Ginger Chody <chodness@gmail.com>
2026-02-07 11:26:04 -0800

    ResArrangeParam "padding" actually holds data?

--- GLink/Immutable/ResArrangeParam.cs
+++ GLink/Immutable/ResArrangeParam.cs
@@ -12,9 +12,10 @@ public partial struct ResArrangeParam
     [FieldOffset(0)] private IntUnion _namePos;
     [FieldOffset(4)] public byte unk4;
     [FieldOffset(5)] public byte unk5;
-    [FieldOffset(6)] [DoNotReverse] private ushort _padding;
+    [FieldOffset(6)] public byte unk6;
+    [FieldOffset(7)] public byte unk7;
 
     public string Name(ref StringTable table) => table.GetByOffset(_namePos);
 
-    public string ToString(ref StringTable table) => $"{table.GetByOffset(_namePos)}: {{ unk4: {unk4}, unk5: {unk5} }}";
+    public string ToString(ref StringTable table) => $"{table.GetByOffset(_namePos)}: {{ unk4: {unk4}, unk5: {unk5}, unk6: {unk6}, unk7: {unk7} }}";
 }
