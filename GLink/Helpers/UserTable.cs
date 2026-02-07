e06f34a11723b586c9beee5d2c131fb1c456a9fa
Ginger Chody <chodness@gmail.com>
2026-02-06 20:10:49 -0800

    Set reader position to end of user table when parsing

--- GLink/Helpers/UserTable.cs
+++ GLink/Helpers/UserTable.cs
@@ -4,10 +4,21 @@ using Revrs;
 namespace GLink.Helpers;
 
 // I hate this. I hate everything about this
-public readonly unsafe ref struct UserTable(ref RevrsReader reader, int start, int end, int numUserParam)
+public readonly unsafe ref struct UserTable
 {
-    private readonly Endianness _endian = reader.Endianness;
-    public Span<byte> Data { get; } = reader.Data[start..end];
+    private readonly Endianness _endian;
+    public Span<byte> Data { get; }
+    private readonly int start;
+    private readonly int numUserParam;
+
+    public UserTable(ref RevrsReader reader, int start, int end, int numUserParam)
+    {
+        this.start = start;
+        this.numUserParam = numUserParam;
+        _endian = reader.Endianness;
+        Data = reader.Data[start..end];
+        reader.Position = end;
+    }
 
     public ResUserData Get(int offset)
     {
